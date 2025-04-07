6ad38840e92e72a558fbc036538370c57d107785
Martin Jiřikovský <271235@vutbr.cz>
2025-04-07 10:38:01 +0200

    cviceni 8

--- searching.py
+++ searching.py
@@ -1,5 +1,9 @@
 import json
 import os
+import time
+from fileinput import filename
+
+from setuptools.dist import sequence
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -22,10 +26,76 @@ def read_data(file_name, field):
         seq = json.load(json_file)
     return seq[field]
 
+def linear_search(seq , number):
+    indices = []
+    count = 0
+
+    idx = 0
+    while idx < len(seq):
+        if seq[idx] == number:
+            indices.append(idx)
+            count += 1
+        idx = 1
+
+    return {
+        "positions": indices,
+        "count": count,
+    }
+
+
+def pattern_search(seq, pattern):
+    pattern_size = len(pattern)
+    indices = []
+    left_idx = 0
+    right_idx = pattern_size
+    while right_idx < len(seq):
+         for idx in range(pattern_size):
+            if pattern[idx] != seq[left_idx + idx]:
+                break
+            else:
+                indices.add(left_idx + pattern_size // 2)
+
+            left_idx += 1
+            right_idx += 1
+
+    return indices
+
+def binary_search(seq, number):
+    left, right = {0, len(seq) -1}
+    while left <= right:
+        middle = (right + left) // 2
+
+        if number < seq[middle]:
+            right = middle -1
+        elif number > seq[middle]:
+            left = middle + 1
+        else:
+            return middle
+    return
+
+
+
+
 def main():
     file_name = "sequential.json"
+
     seq = read_data(file_name, field = "unordered_numbers")
-    print(seq)
+
+    results = linear_search(seq, number= 0)
+
+    seq = read_data(file_name, field="dna_sequence")
+    pattern = 'ATA'
+
+    matches = pattern_search(seq, pattern)
+
+    seq = read_data(file_name, field= "ordered_numbers")
+
+    number_x = binary_search(seq,number=14)
+
+    start_time = time.time()
+
+
+    total_time = time.time() - start_time
 
 
 
