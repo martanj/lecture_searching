84df9bea2891d69292e1ca8e11fa4149af3798d1
Martin Jiřikovský <271235@vutbr.cz>
2025-04-07 09:22:16 +0200

    fgh

--- searching.py
+++ searching.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 # get current working directory path
@@ -11,11 +12,21 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+
+    if field not in {'unordered_numbers', 'ordered_numbers', 'dna_sequence'}:
+        return None
+
     file_path = os.path.join(cwd_path, file_name)
 
+    with open(file_path, "r") as json_file:
+        seq = json.load(json_file)
+    return seq[field]
 
 def main():
-    pass
+    file_name = "sequential.json"
+    seq = read_data(file_name, field = "unordered_numbers")
+    print(seq)
+
 
 
 if __name__ == '__main__':
